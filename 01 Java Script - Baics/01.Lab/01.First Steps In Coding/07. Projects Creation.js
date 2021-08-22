726e388764b55eff1be895e6f368a73474aa9e05
Теодор Вълчев <teo.thenx@gmail.com>
2021-08-22 21:52:33 +0300

    remove

--- 01 Java Script - Baics/01.Lab/01.First Steps In Coding/07. Projects Creation.js
+++
@@ -1,9 +0,0 @@
-function projectsPerHour(input) {
-    let name = input[0];
-    let projectCount = Number(input[1]);
-    let neededHours = projectCount*3;
-
-    console.log(`The architect ${name} will need ${neededHours} hours to complete ${projectCount} project/s.`)
-
-}
-projectsPerHour(["George","4"])
