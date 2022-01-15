135f76d2cb4312bb94b69eb36a517eda8b52e7c5
Теодор Вълчев <teo.thenx@gmail.com>
2022-01-15 13:46:19 +0200

    Formatting

--- 02. JS Advanced/01. Lab/01. JS-Advanced-Syntax-Functions-and-Statements/Echo Function.js
+++ 02. JS Advanced/01. Lab/01. JS-Advanced-Syntax-Functions-and-Statements/Echo Function.js
@@ -1,6 +1,6 @@
-function solve(input){
-    let length = input.length;
-    console.log(length);
-    console.log(input);
+function solve(input) {
+  let length = input.length;
+  console.log(length);
+  console.log(input);
 }
-solve('Hello, JavaScript!');
+solve("Hello, JavaScript!");
